d066b1a532b4fbfd9016b2800a218b81cd5d0471
Harshit <harshitsinghrajput242004@gmail.com>
2025-11-13 01:27:36 +0530

    Installed React-Router-Dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,13 @@
-
+import React from 'react';
+import Landing from './pages/Landing'
+import Navbar from './components/Navbar';
+import { Routes, Route } from "react-router-dom";
 import './App.css'
-
 function App() {
-
 
   return (
     <>
+      <Landing/>
     </>
   )
 }
