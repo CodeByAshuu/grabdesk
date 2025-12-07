5451f02a2cc7a6c0dac5cdad4840b9bc0e0ff5db
Harshit <harshitsinghrajput242004@gmail.com>
2025-12-07 22:16:27 +0530

    feat: cart page UI almost complete (80%)

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -6,7 +6,7 @@ function Button({labell,className,text}){
          border-2 border-[#452215]  bg-[#F0A322] 
          shadow-[4px_4px_0_#452215] 
          active:shadow-none active:translate-x-1 active:translate-y-1 
-         transition-all duration-150 ${className} `}>
+         transition-all duration-150 ${className} transition-all duration-300 hover:shadow-[6px_6px_0_#452215] hover:-translate-y-1 mx-auto`}>
                         <span className={`text-lg nunito-bold ${text}`}>{labell}</span>
                     </button>
         
