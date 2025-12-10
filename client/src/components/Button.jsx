bf4261d4a95b20e0acc98cd65f9d2535ce77eeb0
Sagar <sagarsahu5976@gmail.com>
2025-12-10 15:40:34 +0530

    Connected @ProductCard to @ProductDetail

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,15 +1,17 @@
 import React from "react";
-function Button({labell,className,text}){
+function Button({ labell, className, text, ...props }) {
     return (
         <>
-        <button className={`text-[#FFE9D5] uppercase w-full py-2 rounded-xl 
+            <button className={`text-[#FFE9D5] uppercase w-full py-2 rounded-xl 
          border-2 border-[#452215]  bg-[#F0A322] 
          shadow-[4px_4px_0_#452215] 
          active:shadow-none active:translate-x-1 active:translate-y-1 
-         transition-all duration-150 ${className} transition-all duration-300 hover:shadow-[6px_6px_0_#452215] hover:-translate-y-1 mx-auto`}>
-                        <span className={`text-lg nunito-bold ${text}`}>{labell}</span>
-                    </button>
-        
+         transition-all duration-150 ${className} transition-all duration-300 hover:shadow-[6px_6px_0_#452215] hover:-translate-y-1 mx-auto`}
+                {...props}
+            >
+                <span className={`text-lg nunito-bold ${text}`}>{labell}</span>
+            </button>
+
         </>
     )
 }
