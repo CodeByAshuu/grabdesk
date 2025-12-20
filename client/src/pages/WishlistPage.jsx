38ba0f0e36f2f5ac76fc4136b0b9125d3a87d076
Sagar <sagarsahu5976@gmail.com>
2025-12-21 00:31:30 +0530

    Added and Connected Related products based on category

--- client/src/pages/WishlistPage.jsx
+++ client/src/pages/WishlistPage.jsx
@@ -106,7 +106,7 @@ const WishlistPage = () => {
             </button>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 justify-items-center">
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 justify-items-center">
             {items.map(item => (
               <ProductCard
                 key={item.id}
